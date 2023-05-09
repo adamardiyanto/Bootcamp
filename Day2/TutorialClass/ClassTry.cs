dc9f21fb2a78dd4e837bddf0ebef1503b15f24d5
adamardiyanto <adamardiyantow@gmail.com>
2023-05-09 14:18:04 +0700

    more Tutorial Class

--- Day2/TutorialClass/ClassTry.cs
+++ Day2/TutorialClass/ClassTry.cs
@@ -1,7 +1,7 @@
 ﻿
 class Cat{
     public int age;
-    public string name;
+    //public string name;
     public string race;
 
     public void Eat(){
@@ -18,18 +18,22 @@ class Cat{
         this.race = race;
     }
 
-    public void SetName(string name) {   this.name = name; }
+    
     public void SetRace(string race) {   this.race = race; }
     public void SetAge(int age) {   this.age = age; }
     public int GetAge() { return this.age;  }
-    public string GetName() { return this.name; }
     public string GetRace() { return this.race; }
+
+    public string name{get; set; }
 }
 
 static class Program{
     static void Main(string[] args){
         Cat cotton = new Cat("cotton", 2,"anggora");
         Cat milo = new Cat("milo",1,"kampung");
+        cotton.name = "coto";
+        string value = cotton.name;
+        Console.WriteLine(value);
         cotton.Eat();
         cotton.Sleep();
         milo.Eat();
