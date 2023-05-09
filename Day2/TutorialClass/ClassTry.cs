5a2ddf6c141e90048e1dd4686ad6ceb0abac3e83
adamardiyanto <adamardiyantow@gmail.com>
2023-05-09 14:27:19 +0700

    more tutorial class

--- Day2/TutorialClass/ClassTry.cs
+++ Day2/TutorialClass/ClassTry.cs
@@ -1,39 +1,40 @@
 ﻿
 class Cat{
-    public int age;
-    //public string name;
-    public string race;
+    //variabel getter setter
+    public string _name{get; set; }
+    public int _age{get; set; }
+    public string _race{get; set; }
 
+    //method
     public void Eat(){
-        Console.WriteLine(this.name + " eat");
+        Console.WriteLine(_name + " eat");
     }
 
     public void Sleep(){
-        Console.WriteLine(this.name + " sleep");
+        Console.WriteLine(_name + " sleep");
     }
 
+    //constructor
     public Cat(string name, int age, string race){
-        this.name = name;
-        this.age = age;
-        this.race = race;
+        _name = name;
+        _age = age;
+        _race = race;
+        Console.WriteLine("cat name: " + _name + " age: " + _age + " race: " + _race);
     }
 
     
-    public void SetRace(string race) {   this.race = race; }
-    public void SetAge(int age) {   this.age = age; }
-    public int GetAge() { return this.age;  }
-    public string GetRace() { return this.race; }
-
-    public string name{get; set; }
+    
 }
 
 static class Program{
     static void Main(string[] args){
-        Cat cotton = new Cat("cotton", 2,"anggora");
-        Cat milo = new Cat("milo",1,"kampung");
-        cotton.name = "coto";
-        string value = cotton.name;
-        Console.WriteLine(value);
+        Cat cotton = new Cat("cotton", 2,"anggora"); //instantiate cotton
+        Cat milo = new Cat("milo",1,"kampung"); //instantiate milo
+        cotton._name = "coto"; //try to set new name
+        string value = cotton._name; //try to get the name
+        Console.WriteLine(value); // print the value
+
+        // calling method
         cotton.Eat();
         cotton.Sleep();
         milo.Eat();
