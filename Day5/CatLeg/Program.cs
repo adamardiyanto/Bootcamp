c2994b659c4b27a8930465ba3ba27df17552c56b
adamardiyanto <adamardiyantow@gmail.com>
2023-05-22 13:45:41 +0700

    Day5 about class that can be data type

---
+++ Day5/CatLeg/Program.cs
@@ -0,0 +1,25 @@
+﻿class Cat{
+    public Leg totalLeg;
+
+    public void SetLeg (Leg x){
+        totalLeg = x;
+    }
+}
+
+class Leg{
+    public int LegSize;
+    public Leg (int x){
+        LegSize= x;
+    }
+
+}
+
+class Program{
+    public static void Main(String[] args){
+        Cat milo = new Cat();
+        Leg catLeg = new Leg(4);
+        milo.SetLeg(catLeg);
+        Console.WriteLine(milo.totalLeg.LegSize );
+    
+    }
+}
