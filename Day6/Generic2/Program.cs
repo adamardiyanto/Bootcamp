65cab8dd97f71cac29dea2dda7797ff49d8b49dc
adamardiyanto <adamardiyantow@gmail.com>
2023-05-23 20:21:34 +0700

    continue learn generic

--- Day6/Generic2/Program.cs
+++ Day6/Generic2/Program.cs
@@ -1,22 +1,31 @@
-﻿public class GenericOlshop{
-    public private ProductFood internalProduct;
-    public ProductFood DisplayProduct<T, T2, T3> (T a, T b, T c){
+﻿static public class GenericOlshop{
+    public static ProductFood internalProduct;
+    public static ProductFood? DisplayProduct<T, T2, T3> (T a, T2 b, T3 c){
         if (a is string keyOne && b is int keyTwo){
             ProductFood myProductFood =new();
-            myProductFood.name = keyOne;
-            myProductFood.price = keyTwo;
+            myProductFood.Name = keyOne;
+            myProductFood.Price = keyTwo;
 
             return myProductFood;
         }
         return null;
     }
 
-    public void SetProduct<T>(T a){
+    public static void SetProduct<T>(T a){
         if(a is ProductFood myProductFood){
             internalProduct = myProductFood;
         }else{
             Console.WriteLine("invalid");
         }
+    }
+
+    public static ProductFood? ProductEditor<T, T2> (T a, T2 b){
+        if(a is string keyOne &&b is int keyTwo){
+            internalProduct.Name = keyOne;
+            internalProduct.Price = keyTwo;
+            return internalProduct;
+        }
+        return null;
     }
 
 
@@ -32,6 +41,13 @@ public class ProductFood {
 
 static class Program{
     static void Main(string[] args){
-        ProductFood myProductFood = GenericOlshop.DisplayProduct<>;
+        ProductFood myProductFood = GenericOlshop.DisplayProduct<string, int, int>("Burger", 5, 34);
+        Console.WriteLine(myProductFood.Name);
+        Console.WriteLine(myProductFood.Price);
+
+        GenericOlshop.SetProduct<ProductFood>(myProductFood);
+        myProductFood = GenericOlshop.ProductEditor<string, int>("Pizaa",1000);
+        Console.WriteLine(myProductFood.Name);
+        Console.WriteLine(myProductFood.Price);
     }
 }
