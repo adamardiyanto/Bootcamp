b8b7e6ca49e6fbe00a7287a7d12e05c1311ada39
adamardiyanto <adamardiyantow@gmail.com>
2023-05-23 17:03:09 +0700

    learn generic

---
+++ Day6/Generic2/Program.cs
@@ -0,0 +1,37 @@
+﻿public class GenericOlshop{
+    public private ProductFood internalProduct;
+    public ProductFood DisplayProduct<T, T2, T3> (T a, T b, T c){
+        if (a is string keyOne && b is int keyTwo){
+            ProductFood myProductFood =new();
+            myProductFood.name = keyOne;
+            myProductFood.price = keyTwo;
+
+            return myProductFood;
+        }
+        return null;
+    }
+
+    public void SetProduct<T>(T a){
+        if(a is ProductFood myProductFood){
+            internalProduct = myProductFood;
+        }else{
+            Console.WriteLine("invalid");
+        }
+    }
+
+
+}
+
+public class ProductFood {
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+    public string Description { get; set; }
+
+}
+
+static class Program{
+    static void Main(string[] args){
+        ProductFood myProductFood = GenericOlshop.DisplayProduct<>;
+    }
+}
---
+++ Day6/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿public class ToolBox<T> {
+    private T _tool;
+
+    public void AddTool(T tool) {
+        _tool = tool;
+    }
+
+    public T GetTool(){
+        return _tool;
+    }
+}
+
+static class Program{
+    static void Main(string[] args){
+        ToolBox<string> drillBox = new();
+        drillBox.AddTool("drill");
+        Console.WriteLine(drillBox.GetTool());
+
+        ToolBox<string> hammerBox = new();
+        hammerBox.AddTool("hammer");
+        Console.WriteLine(hammerBox.GetTool());
+
+        ToolBox<int> totalBox = new();
+        totalBox.AddTool(9);
+        Console.WriteLine(totalBox.GetTool());
+        
+
+    }
+}
