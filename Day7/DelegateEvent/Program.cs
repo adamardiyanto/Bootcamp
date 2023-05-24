bcf783c1bc761458b7269aec7cd3093335b661d1
adamardiyanto <adamardiyantow@gmail.com>
2023-05-24 16:05:22 +0700

    learn delegate event

---
+++ Day7/DelegateEvent/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Publisher{
+    public event EventHandler<string> CommandSent;
+
+    public void SendCommand(string command){
+        CommandSent.Invoke(this, command);
+    }
+}
+
+public class Subscriber{
+    public string nama { get; set; }
+    public void CommandReceived(object sender, string command){
+                Console.WriteLine($"{nama} menerima perintah: {command}");
+
+    }
+    public Subscriber(string x){
+        nama = x;
+    }
+}
+
+public static class Program{
+    public static void Main(string[] args){
+        Publisher publisher = new Publisher();
+        Subscriber siA = new Subscriber("siA");
+        Subscriber siB = new Subscriber("siB");
+
+        publisher.CommandSent += siA.CommandReceived;
+        publisher.CommandSent += siB.CommandReceived;
+
+        publisher.SendCommand("Get Ready");
+    }
+}
