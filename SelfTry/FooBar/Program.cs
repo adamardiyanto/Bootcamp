5f90cbb6b0ecfa5f467b3062aef7736a274b109d
adamardiyanto <adamardiyantow@gmail.com>
2023-05-25 20:05:12 +0700

    try foobar

--- SelfTry/FooBar/Program.cs
+++ SelfTry/FooBar/Program.cs
@@ -1,45 +1,55 @@
-﻿class Program
+﻿using System.Collections.Generic;
+namespace foobarQuiz;
+static class Program
 {
     static void Main(string[] args)
     {
+        Checker checker = new Checker();
         Console.WriteLine("Masukan input");
         int input;
-        int.TryParse((Console.ReadLine()),out input);
+        int.TryParse((Console.ReadLine()), out input);
+        Console.WriteLine(checker.CheckInput(input));
         Console.WriteLine(input);
     }
-
-
 }
 
 class Checker
 {
     private int _number;
-    public void CheckInput(int input)
+    private string result = " ";
+
+    private string foobar = "";
+
+    public string CheckInput(int input)
     {
+        Print print = new Print();
+        Dictionary<int, string> myDict = new Dictionary<int, string>();
+        myDict.Add(3, "foo");
+        myDict.Add(5, "bar");
         _number = input;
         for (int i = 0; i < _number; i++)
         {
-            string foo = FooBar.Foo(i);
-            string bar = FooBar.Bar(i);
-            if (foo != null )
+            foreach (KeyValuePair<int, string> dict in myDict)
             {
-                
+                foobar += print.PrintFoobar(i, dict.Key, dict.Value);
+                //Console.WriteLine($" {print.PrintFoobar(15, dict.Key, dict.Value)} ");
             }
-
+            
+            if (foobar == " ") foobar = _number.ToString();
+            
+            result+= foobar;
+            foobar = " ";
         }
-
+        return result;
     }
 }
 
-class FooBar
+public class Print
 {
-    public string Foo(int input)
+    public string PrintFoobar(int number, int key, string value)
     {
-        if (input % 3 == 0) return "foo";
+        if (number % key == 0) return value;
+        return "";
     }
 
-    public string Foo(int input)
-    {
-        if (input % 3 == 0) return "foo";
-    }
 }
