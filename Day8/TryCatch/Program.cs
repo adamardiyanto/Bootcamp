335929cfe892a18111f7a1318ffeaafaf2b49b08
adamardiyanto <adamardiyantow@gmail.com>
2023-05-25 14:32:35 +0700

    more TryCatch

--- Day8/TryCatch/Program.cs
+++ Day8/TryCatch/Program.cs
@@ -3,19 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        int a = 0;
+        
         try
         {
-            int result = 10/ a;
+            int q = 0;
+            int x = 10 / q; // DivideByZeroException
+            
+            int[] numbers = { 1, 2, 3 };
+            Console.WriteLine(numbers[5]); // IndexOutOfRangeException
+
+
+            string s = null;
+            Console.WriteLine(s.Length); // NullReferenceException
+
+            int y = int.Parse("hello"); // FormatException
         }
-        catch (Exception e)
+        catch (IndexOutOfRangeException e)
         {
             Console.WriteLine(e.Message);
         }
-        // catch (DivideByZeroException e)
-        // {
-        //     Console.WriteLine(e.Message);
-        // }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
         finally
         {
             Console.WriteLine("code excecuted");
