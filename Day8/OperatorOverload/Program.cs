5e646d24d2fd28db1ef912656e92e55402353312
adamardiyanto <adamardiyantow@gmail.com>
2023-05-25 14:14:01 +0700

    TryCatch and Operator Overloading

---
+++ Day8/OperatorOverload/Program.cs
@@ -0,0 +1,58 @@
+﻿public class Fraction 
+{
+    public int atas { get; set; }
+    public int bawah { get; set; }
+
+    public Fraction(int atas, int bawah)
+    {
+        this.atas = atas;
+        this.bawah = bawah;
+    }
+
+    public static Fraction operator + (Fraction a, Fraction b)
+    { 
+        int newAtas;
+        int newBawah;
+
+        newAtas = (a.atas * b.bawah) + (b.atas * a.bawah);
+        newBawah = a.bawah * b.bawah;
+
+        return new Fraction(newAtas, newBawah);
+
+    }
+
+    public static Fraction operator -(Fraction a, Fraction b)
+    {
+        int newAtas;
+        int newBawah;
+
+        newAtas = (a.atas * b.bawah) - (b.atas * a.bawah);
+        newBawah = a.bawah * b.bawah;
+
+        return new Fraction(newAtas, newBawah);
+    }
+
+    public static Fraction operator *(Fraction a, Fraction b)
+    {
+        return new Fraction(a.atas * b.atas, a.bawah * b.bawah);
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fraction pecahan1 = new Fraction(3,5);
+        Fraction pecahan2 = new Fraction(2,5);
+        Fraction pecahan3 = new Fraction(8,5);
+
+
+        Fraction pecahan4 = pecahan1 + pecahan2 + pecahan3;
+        Fraction pecahan6 = pecahan1 * pecahan2;
+        Console.WriteLine($"{pecahan4.atas} {pecahan4.bawah}");
+        Console.WriteLine($"{pecahan6.atas} {pecahan6.bawah}");
+
+    }
+
+}
---
+++ Day8/TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        try
+        {
+            int result = 10/ a;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        // catch (DivideByZeroException e)
+        // {
+        //     Console.WriteLine(e.Message);
+        // }
+        finally
+        {
+            Console.WriteLine("code excecuted");
+        }
+
+    }
+}
