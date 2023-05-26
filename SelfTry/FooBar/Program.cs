02fe4caf63d16e10d6176dd1ac043218af7bc33d
adamardiyanto <adamardiyantow@gmail.com>
2023-05-26 12:34:07 +0700

    more foobar quiz

--- SelfTry/FooBar/Program.cs
+++ SelfTry/FooBar/Program.cs
@@ -9,7 +9,6 @@ static class Program
         int input;
         int.TryParse((Console.ReadLine()), out input);
         Console.WriteLine(checker.CheckInput(input));
-        Console.WriteLine(input);
     }
 }
 
@@ -27,18 +26,18 @@ class Checker
         myDict.Add(3, "foo");
         myDict.Add(5, "bar");
         _number = input;
-        for (int i = 0; i < _number; i++)
+        for (int i = 1; i <= _number; i++)
         {
             foreach (KeyValuePair<int, string> dict in myDict)
             {
-                foobar += print.PrintFoobar(i, dict.Key, dict.Value);
-                //Console.WriteLine($" {print.PrintFoobar(15, dict.Key, dict.Value)} ");
+                bool test = int.TryParse(print.PrintFoobar(i, dict.Key, dict.Value), out int x);
+                foobar = test? i.ToString() : print.PrintFoobar(i, dict.Key, dict.Value);  
+                
             }
             
-            if (foobar == " ") foobar = _number.ToString();
+            Console.WriteLine(foobar);
             
-            result+= foobar;
-            foobar = " ";
+            
         }
         return result;
     }
@@ -49,7 +48,7 @@ public class Print
     public string PrintFoobar(int number, int key, string value)
     {
         if (number % key == 0) return value;
-        return "";
+        else return number.ToString();
     }
 
 }
