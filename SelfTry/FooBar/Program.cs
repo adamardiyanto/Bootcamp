2bc088d8054b2777c408e4a997b4f2c86e7e68b6
adamardiyanto <adamardiyantow@gmail.com>
2023-05-26 16:46:22 +0700

    foobar again

--- SelfTry/FooBar/Program.cs
+++ SelfTry/FooBar/Program.cs
@@ -5,6 +5,8 @@ static class Program
     static void Main(string[] args)
     {
         Checker checker = new Checker();
+        checker.AddDictionary(3, "Foo");
+        checker.AddDictionary(5, "Bar");
         Console.WriteLine("Masukan input");
         int input;
         int.TryParse((Console.ReadLine()), out input);
@@ -19,37 +21,48 @@ class Checker
 
     private string foobar = "";
 
+    Dictionary<int, string> myDict;
+
+    public Checker()
+    {
+        myDict = new Dictionary<int, string>();
+    }
+
     public string CheckInput(int input)
     {
-        Print print = new Print();
-        Dictionary<int, string> myDict = new Dictionary<int, string>();
-        myDict.Add(3, "foo");
-        myDict.Add(5, "bar");
         _number = input;
+
+
         for (int i = 1; i <= _number; i++)
         {
-            foreach (KeyValuePair<int, string> dict in myDict)
-            {
-                // bool test = int.TryParse(print.PrintFoobar(i, dict.Key, dict.Value), out int x);
-                // foobar = test? i.ToString() : print.PrintFoobar(i, dict.Key, dict.Value);  
-                result += print.PrintFoobar(i, dict.Key, dict.Value);
-                
-            }
+            Console.WriteLine(CheckSingleNumber(i));
             
-            //Console.WriteLine(foobar);
-            
+            //result += tempResult;
             
         }
         return result;
     }
-}
-
-public class Print
-{
+    
     public string PrintFoobar(int number, int key, string value)
     {
         if (number % key == 0) return value;
-        else return number.ToString();
+        else return " ";
+    }
+
+    public void AddDictionary(int key, string value)
+    {
+        myDict.Add(key, value);
+    }
+
+    public string CheckSingleNumber(int input)
+    {
+        string result = " ";
+        foreach (KeyValuePair<int, string> dict in myDict)
+            {
+                result += PrintFoobar(input, dict.Key, dict.Value);
+            }
+        return result;
     }
 
 }
+
