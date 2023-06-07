4ada838f5f7f11c91b9bbd7af94b83609bc816f1
adamardiyanto <adamardiyantow@gmail.com>
2023-06-07 16:44:27 +0700

    learn about Garbage Collection

---
+++ Day15/Destructor/Program.cs
@@ -0,0 +1,29 @@
+﻿public class TryDestructor
+{
+    public TryDestructor()
+    {
+        Console.WriteLine("Object created");
+    }
+    ~TryDestructor()
+    {
+        Console.WriteLine("Object destroyed");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TryDestructor obj1 = new TryDestructor();
+        TryDestructor obj2 = new TryDestructor();
+
+        
+
+        for(int i = 0; i< 1000000000; i++)
+        {
+            obj1 = null;
+            GC.Collect();
+            //int a = i;
+        }
+    }
+}
---
+++ Day15/Disposable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+class UsingStatement
+{
+    static void Main()
+    {
+        var buffer = new char[50];
+        using (StreamReader streamReader = new("file.txt"))
+        {
+            int charsRead = 0;
+            while (streamReader.Peek() != -1)
+            {
+                charsRead = streamReader.Read(buffer, 0, buffer.Length);
+                Console.WriteLine(streamReader.ReadLine());
+            }
+        }
+    }
+}
