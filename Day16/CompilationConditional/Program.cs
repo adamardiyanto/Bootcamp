adcf035d6501894700dfcd1a6721aab7bb863caa
adamardiyanto <adamardiyantow@gmail.com>
2023-06-08 14:06:27 +0700

    conditional compilation

---
+++ Day16/CompilationConditional/Program.cs
@@ -0,0 +1,12 @@
+﻿#define RELEASE
+class Program {
+	static void Main() {
+		#if (DEBUG)
+		Console.WriteLine("Mode debug diaktifkan.");
+		#elif (RELEASE)
+		Console.WriteLine("Production mode aktif");
+		#endif
+
+		Console.WriteLine("Program berjalan...");
+	}
+}
