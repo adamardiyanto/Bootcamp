45e4a686cb2466392611fd28b408538930fdeca3
adamardiyanto <adamardiyantow@gmail.com>
2023-06-14 16:33:40 +0700

    learn serialization deserialization

---
+++ Day20/XMLSerialization/Program.cs
@@ -0,0 +1,44 @@
+﻿﻿using System.IO;
+using System.Xml.Serialization;
+public class Animal
+{
+    public int age { get; set; }
+    public string name { get; set; }
+    public Animal(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public Animal(){}
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal can make own sounds");
+    }
+}
+public class Cat : Animal
+{
+    public Cat(string name, int age) : base(name, age)
+    {
+        Console.WriteLine("Cat with name " + name + " age "+age+" created");
+    }
+    public Cat(){}
+    public override void MakeSound() {
+        Console.WriteLine("Cat Meowing sound");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+    Cat cat1 = new Cat("puss",1);
+    Cat cat2 = new Cat("meng",2);
+    Animal hewan = new Animal("hewan",12);
+    XmlSerializer serializer = new XmlSerializer(typeof(Cat));
+    using(StreamWriter writer = new StreamWriter("cat.xml"))
+        {
+            serializer.Serialize(writer, cat1);
+            serializer.Serialize(writer, cat2);
+            serializer.Serialize(writer, hewan);
+        }
+    }
+}
