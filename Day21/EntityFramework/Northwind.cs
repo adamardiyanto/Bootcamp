ad002188fb747c6349225bfcfc8c2ce51c521c44
adam ardiyanto <adamardiyantow@gmail.com>
2023-06-23 16:29:19 +0700

    learn entity framework with sqlite

---
+++ Day21/EntityFramework/Northwind.cs
@@ -0,0 +1,15 @@
+using EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDatabase;
+
+public class Northwind : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+        string connection = ($"Filename={path}");
+        Console.WriteLine($"Connecting to {connection}");
+        optionsBuilder.UseSqlite(connection);
+    }
+}
---
+++ Day21/EntityFramework/Program.cs
@@ -0,0 +1,11 @@
+﻿using EFDatabase;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Northwind db = new Northwind();
+        Console.WriteLine($"database name : {db.Database}");
+        Console.WriteLine($"provider name : {db.Database.ProviderName}");
+    }
+}
